038b3efc13a638bcca074cea9d12790bf888fb91
HernandoR <lzhen.dev@outlook.com>
2025-05-02 20:03:22 +0800

    prepare tools and decorator for compative with list input
    Signed-off-by: HernandoR <lzhen.dev@outlook.com>

---
+++ pytransform3d/array_api.py
@@ -0,0 +1,26 @@
+import array_api_compat as xarray
+import numpy as np
+from functools import wraps
+
+def _np_arraylize_if_list(*args):
+    return [
+        np.asarray(arg) if isinstance(arg, list) else arg
+        for arg in args
+    ]
+
+def get_namespace(*args, **kwargs):
+    """
+    Get the array namespace of the position arguments.
+    
+    for list instance, return numpy.
+    """
+    args = _np_arraylize_if_list(*args)
+    return xarray.array_namespace(*args, **kwargs)
+
+# decorator to transfer list args to numpy
+def np_arraylize_if_list(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        args = _np_arraylize_if_list(*args)
+        return func(*args, **kwargs)
+    return wrapper
